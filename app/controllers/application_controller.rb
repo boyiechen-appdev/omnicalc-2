5f65942c60c770c247ce87dee38a7b4d4f808b7a
boyiechen <boyie@uchicago.edu>
2023-04-11 15:38:14 +0000

    finish addition

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
 
+  # addition form
+  def add
+    render({ :template => "calculation_templates/add.html.erb" })
+  end
+
+  # addition result
+  def wizard_add
+    @num_1 = params.fetch("first_num").to_f
+    @num_2 = params.fetch("second_num").to_f
+    @result = @num_1 + @num_2
+    render({ :template => "calculation_templates/add_result.html.erb" })
+  end
 end
