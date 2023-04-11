1a4e8239af0c50909df028c910a652feb355a04d
boyiechen <boyie@uchicago.edu>
2023-04-11 16:30:56 +0000

    finish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,4 +25,30 @@ class ApplicationController < ActionController::Base
     @result = @num_1 - @num_2
     render({ :template => "calculation_templates/subtract_result.html.erb" })
   end
+
+  # multiplication form
+  def multiply
+    render({ :template => "calculation_templates/multiply.html.erb" })
+  end
+
+  # multiplication result
+  def wizard_multiply
+    @num_1 = params.fetch("first_num").to_f
+    @num_2 = params.fetch("second_num").to_f
+    @result = @num_1 * @num_2
+    render({ :template => "calculation_templates/multiply_result.html.erb" })
+  end
+
+  # division form
+  def divide
+    render({ :template => "calculation_templates/divide.html.erb" })
+  end
+
+  # division result
+  def wizard_divide
+    @num_1 = params.fetch("first_num").to_f
+    @num_2 = params.fetch("second_num").to_f
+    @result = @num_1 / @num_2
+    render({ :template => "calculation_templates/divide_result.html.erb" })
+  end
 end
